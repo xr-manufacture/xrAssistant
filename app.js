28645ce9f7aa1aab50766c8dad6bc454b6d68fab
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-05-01 01:08:17 -0400

    Added a workable scene for webXR app dev

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ class App{
 		document.body.appendChild( container );
 
 		// write code here
+		this camera = new THREE.PerspectiveCamera(
+			60, 
+			window.innerWidth/window.innerHeight,
+			0.1,
+			1000
+		);
+		this.camera.position.set(0,0,4);
 
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0xaaaaaa);
+
+		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		container.appendChild(this.renderer.domElement);
+
+		this.renderer.setAnimationLoop(this.render.bind(this));
 		
-
 		window.addEventListener('resize', this.resize.bind(this));
 	}
 
@@ -22,7 +37,7 @@ class App{
 	}
 
 	render(){
-	
+		this.renderer.render(this.scene, this.camera);
 	}
 }
 
