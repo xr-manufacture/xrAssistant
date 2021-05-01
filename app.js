53daa1c36ae25e3466ad300ea11e98f87cb4699e
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-05-01 00:57:40 -0400

    Added a three.js ES6 modular template

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-import * from './Assets/Scripts/three.js-master/ex'
+import * as THREE from './Assets/Scripts/THREE/build/three.module.js';
+import {OrbitControls} from './Assets/Scripts/THREE/examples/js/controls/OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement('div');
+		document.body.appendChild( container );
+
+		// write code here
+
+		
+
+		window.addEventListener('resize', this.resize.bind(this));
+	}
+
+	setupVR(){
+	
+	}
+	
+	resize(){
+	
+	}
+
+	render(){
+	
+	}
+}
+
+export { App };
