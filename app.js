8e2caad4a29868856ecbe5e0defbeaf3bffa879d
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-05-01 01:50:55 -0400

    Added a test cube for 3d scene

--- app.js
+++ app.js
@@ -18,12 +18,33 @@ class App{
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color(0xaaaaaa);
 
+		const ambient = new THREE.HemisphereLight(
+			0xffffff,
+			0xbbbbff,
+			0.3
+		);
+		this.scene.add(ambient);
+
+		const light = new THREE.DirectionalLight();
+		light.position.set(0.2, 1, 1);
+		this.scene.add(light);
+
 		this.renderer = new THREE.WebGLRenderer({antialias: true});
 		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		container.appendChild(this.renderer.domElement);
 
 		this.renderer.setAnimationLoop(this.render.bind(this));
+
+		// Add geometries
+		const geometry = new THREE.BoxBufferGeoemtry();
+		const material = new THREE.MeshStandardMaterial({
+			color: 0xff0000
+		});
+		this.mesh = new THREE.Mesh(geometry, material);
+		this.scene.add(this.mesh);
+
+		const controls = new OrbitControls(this.camera, this.renderer.domElement);
 		
 		window.addEventListener('resize', this.resize.bind(this));
 	}
@@ -33,10 +54,13 @@ class App{
 	}
 	
 	resize(){
-	
+		this.camera.aspect = window.innerWidth/window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
 	}
 
 	render(){
+		this.mesh.rotateY(0.01);
 		this.renderer.render(this.scene, this.camera);
 	}
 }
