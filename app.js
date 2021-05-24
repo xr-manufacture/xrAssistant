ebb91778da1f01a3de99681c4fedd99c129bfee6
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-05-24 03:42:03 -0400

    Change hand model to sphere model

--- app.js
+++ app.js
@@ -157,12 +157,12 @@ function setupXR() {
 
 	// Hand 1
 	hand1 = renderer.xr.getHand(0);
-	hand1.add(handModelFactory.createHandModel(hand1, "oculus"));
+	hand1.add(handModelFactory.createHandModel(hand1, "sphere"));
 	scene.add(hand1);
 
 	// Hand 2
 	hand2 = renderer.xr.getHand(1);
-	hand2.add(handModelFactory.createHandModel(hand2, "oculus"));
+	hand2.add(handModelFactory.createHandModel(hand2, "sphere"));
 	scene.add(hand2);
 
 	// Handle controllers/hands
